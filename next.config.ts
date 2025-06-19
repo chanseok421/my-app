9256538436413cfb5d175be7434e89ead3fa3796
chanseok421 <dl9704tjr@gmail.com>
2025-06-19 15:11:17 +0900

    [FEAT] 서버 성공

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://172.21.100.174:3000"],
+
+  experimental: {
+    // 여기에 다른 실험 옵션이 있다면 추가 가능
+  },
 };
 
 export default nextConfig;
